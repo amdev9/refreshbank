8005fa8b5a1860fe429043c6c49091756ff2847d
Alexander Matveev <blackkorol@gmail.com>
2015-05-10 22:17:59 +0300

    asdasd

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :microcredits   
+  resources :microcredits  , :only => [:index, :show] 
 
-   resources :creditcards
+   resources :creditcards , :only => [:index, :show]
     root :to => "microcredits#index"
    # ,  :path => '/'
-  # :only => [:index, :show],
+  :only => [:index, :show],
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
