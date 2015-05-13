bf5007f60581d85bdec9f829ce3f7a4442a7969e
Alexander Matveev <blackkorol@gmail.com>
2015-05-13 23:14:28 +0400

    asdasd

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -76,6 +76,7 @@ class CreditcardsController < ApplicationController
       respond_to do |format|
     format.xml { render file: 'public/sitemaps/sitemap.xml' }
     format.html { redirect_to root_url }
-  end
+  		end
+	end
 
 end
