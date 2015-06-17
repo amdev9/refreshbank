a5211a7198876f1c55b93cdcb13f995142208963
Alexander Matveev <blackkorol@gmail.com>
2015-06-17 22:04:33 +0400

    asdasd

--- app/assets/javascripts/js.js
+++ app/assets/javascripts/js.js
@@ -4354,6 +4354,48 @@ function() {
         })
     });
     
+
+    jQuery (function() {
+   $.fn.fixMe = function() {
+      return this.each(function() {
+         var $this = $(this),
+            $t_fixed;
+         function init() {
+           
+            $t_fixed = $this.clone();
+            $t_fixed.find("tbody").remove().end().addClass("fixed").insertBefore($this);
+            resizeFixed();
+         }
+         function resizeFixed() {
+            $t_fixed.find("th").each(function(index) {
+               $(this).css("width",$this.find("th").eq(index).outerWidth()+"px");
+            });
+         }
+         function scrollFixed() {
+            var offset = $(this).scrollTop(),
+            tableOffsetTop = $this.offset().top,
+            tableOffsetBottom = tableOffsetTop + $this.height() - $this.find("thead").height();
+            if(offset < tableOffsetTop || offset > tableOffsetBottom)
+               $t_fixed.hide();
+            else if(offset >= tableOffsetTop && offset <= tableOffsetBottom && $t_fixed.is(":hidden"))
+               $t_fixed.show();
+         }
+         $(window).resize(resizeFixed);
+         $(window).scroll(scrollFixed);
+         init();
+      });
+   };
+});
+
+$(document).ready(function(){
+   $("table").fixMe();
+   $(".up").click(function() {
+      $('html, body').animate({
+      scrollTop: 0
+   }, 2000);
+ });
+});
+
     // function() {
     //     jQuery(function() {
     //         return $(".tooltip").tooltip(), $('[rel="tooltip"]').tooltip(), $('[data-toggle="tooltip"]').tooltip()
