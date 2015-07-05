d65c0bfc0aa8a093d8b188351f2e31f8b019d724
Alexander Matveev <blackkorol@gmail.com>
2015-07-05 20:18:31 +0300

    cavk

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-	def canonical(url)
-		content_for (:canonical, tag(:link, :rel => :canonical, :href => url)) if url
-	end
+	# def canonical(url)
+	# 	content_for (:canonical, tag(:link, :rel => :canonical, :href => url)) if url
+	# end
 end
