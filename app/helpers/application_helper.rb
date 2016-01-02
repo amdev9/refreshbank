5629761b0bbf5f522e7662b06732e498c29d6465
Александр Матвеев <alex@MacBook-Pro-Aleksandr.local>
2016-01-02 22:27:24 +0300

    seo tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
 	def canonical(url)	 
 		content_for :canonical, tag(:link, :rel => :canonical, :href => url)  
 	end
+
+	def meta_tag(tag, text)
+   	 content_for :"meta_#{tag}", text
+  	end
+
+    def yield_meta_tag(tag, default_text='')
+      content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
+    end
+
 end
